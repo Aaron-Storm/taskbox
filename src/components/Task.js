f62747f77555cc1fddce53833a5f47b1f22c39ae
Aaron <60785649+Aaron-Storm@users.noreply.github.com>
2020-09-23 01:30:07 +0700

    changed background

--- src/components/Task.js
+++ src/components/Task.js
@@ -19,7 +19,7 @@ export default function Task({ task: { id, title, state}, onArchiveTask, onPinTa
             value={title}
             readOnly={true}
             placeholder="Input title"
-            style={{ background: 'white'}}
+            style={{ background: 'blue'}}
           />
         </div>
         <div className="actions" onClick={(event) => event.stopPropagation()}>
